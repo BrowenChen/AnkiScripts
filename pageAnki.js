cc89cd38e41a57f39efbdc501ea0b4e5186f9ba5
Browenchen <owenchen@berkeley.edu>
2014-06-17 20:08:53 +0100

    New idea, pageParse

---
+++ pageAnki.js
@@ -0,0 +1,28 @@
+/**
+ pageAnki.js
+ Problem: Read a ton of information but we aren’t able to retain the information. 
+ Solutions: Parse information with simple design. Anki spaced repetition format to store into a card bank to retain the information over the long term
+ Habit Idea: Everyday learn a new fact. Cycle between topics in order to maximize exposure. 
+ Idea: Hold down a button, such as control, and highlight a text. This parses that text into the front card and appends a “:” to it. Next, highlight the next bit of text and that filters out the key words to make the second section
+ **/
+
+ //Parses data into JSON file? 
+
+
+ /*
+	Method to filter out filler words.
+	Contains a list of filler words, loops through text and replaces 
+	words that match with a comma.
+*/
+
+function filter(word){
+	//Filter
+}
+
+/*
+	Action start listening. When stopped, take the highlighted input and 
+	put it into a variable object.
+*/
+
+
+
