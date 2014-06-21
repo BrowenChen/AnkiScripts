e231e4a7fb365b8dacbad5c8c6718fe706f6b443
Browenchen <owenchen@berkeley.edu>
2014-06-21 11:30:09 +0100

    Scraping works
    Takes pdf files and downloads onto same directory as scoreScraper. also
    opens up the tabs

--- scoreScrape/scoreScraper.py
+++ scoreScrape/scoreScraper.py
@@ -1,20 +1,32 @@
 # For samir
 import webbrowser
-
-url = "http://google.com"
-
-def open():
-	return webbrowser.open("http://google.com", new=2, autoraise=True)
-
-# webbrowser.open_new(url)
-webbrowser.open_new_tab(url)
-
-# scoreUrl = "http://openeducationchallenge.eu/sites/default/files/Phase3Results/OECPhase3Results_" + num + ".pdf"
-
-def scoreSheet():
-	for x in range(65, 80):
+import pdfminer
+from pattern.web import URL
+#open up the url's
+def scoreSheet(x, y):
+	for x in range(x, y):
+		store(x)
 		scoreUrl = "http://openeducationchallenge.eu/sites/default/files/Phase3Results/OECPhase3Results_" + str(x) + ".pdf"
 		webbrowser.open_new_tab(scoreUrl)
 
 
+#Now download and store them into samples/ folder
 
+def store(num):
+	scoreUrl = "http://openeducationchallenge.eu/sites/default/files/Phase3Results/OECPhase3Results_" + str(num) + ".pdf"
+
+	url = URL(scoreUrl)
+	f = open("OED" + str(num) + ".pdf", 'wb')
+	f.write(url.download(cached=False))
+	f.close()
+	print("Finished downloading " + str(num))
+	#
+
+#These are stored in the same folder as scoreScraper.py
+
+#Parse the data
+
+
+
+
+scoreSheet(65, 80)
