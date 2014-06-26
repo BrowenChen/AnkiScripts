473adfca93b2476536df328577315a6203fb53c1
Browenchen <owenchen@berkeley.edu>
2014-06-26 11:29:40 +0100

    csv to evernote. Incremental Reading

---
+++ csv2evr.py
@@ -0,0 +1,10 @@
+#csv2evr.py
+#------------
+#
+#
+#Export deck from Anki Browser into CSV format.
+#Parse CSV into a note to plug into evernote.
+#We want =>
+# for row in CSV:
+#	 \t, csv[1] + ":" csv[2] \n 
+
